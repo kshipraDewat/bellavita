16a09c20eab74e53aca3c5ec3e0616bdbb1a0686
kshipradewat <kshipradewat66@gmail.com>
2025-02-08 14:53:55 +0530

    changes 1

--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,3 @@
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
@@ -6,11 +5,11 @@ import { BrowserRouter } from 'react-router'
 import { ProductContextProvider } from './context/ProductContext.jsx'
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
+ 
     <BrowserRouter>
     <ProductContextProvider>
       <App />
     </ProductContextProvider>
     </BrowserRouter>
-  </StrictMode>,
+  
 )
